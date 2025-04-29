6c0bf42e237019f568add75bbb27de001bec65b2
kip2 <tqa1no4@gmail.com>
2025-04-29 14:49:45 +0900

    Finished making game

--- guess_the_number/lib/guess_the_number.ex
+++ guess_the_number/lib/guess_the_number.ex
@@ -1,4 +1,31 @@
 defmodule Main do
+  def main do
+    # The answer number.
+    answer = generate_random_number()
+
+    # game start
+    start_game(answer)
+  end
+
+  def start_game(answer) do
+    # User input number.
+    user_input = read_input()
+
+    # Number judge.
+    case judge(user_input, answer) do
+      :correct ->
+        IO.puts("正解! おめでとう!")
+
+      :too_low ->
+        IO.puts("数字が小さいです")
+        start_game(answer)
+
+      :too_high ->
+        IO.puts("数字が大きいです")
+        start_game(answer)
+    end
+  end
+
   def generate_random_number do
     1..100 |> Enum.random()
   end
@@ -15,8 +42,11 @@ defmodule Main do
     end
   end
 
-  def main do
-    n = read_input() |> Integer.to_string()
-    IO.puts("あなたの入力した数字は、" <> n <> " です。")
+  def judge(input, answer) do
+    cond do
+      input == answer -> :correct
+      input < answer -> :too_low
+      input > answer -> :too_high
+    end
   end
 end
